658e66e50a5d86344bc184970895baff4533e788
raghav1919 <raghav.goud08@gmail.com>
2018-11-15 15:07:58 +0530

    Latest version

--- RaghavFirstAngular/Controllers/HomeController.cs
+++ RaghavFirstAngular/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace RaghavFirstAngular.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Welcome to Application.";
 
             return View();
         }
